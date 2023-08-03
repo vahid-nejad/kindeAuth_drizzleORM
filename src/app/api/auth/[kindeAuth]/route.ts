3ecde448b42074271ab7ec9be2604b9989a56407
vahid-nejad <vahid884@gmail.com>
2023-08-04 00:32:11 +0330

    first commit

--- src/app/api/auth/[kindeAuth]/route.ts
+++
@@ -1,6 +0,0 @@
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-
-export async function GET(request: any, { params }: { params: any }) {
-  const endpoint = params.kindeAuth;
-  return handleAuth(request, endpoint);
-}
