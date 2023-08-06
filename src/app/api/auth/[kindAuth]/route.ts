02365545d086e7aef86435b456bc97966a02435f
vahid-nejad <vahid884@gmail.com>
2023-08-06 12:49:05 +0330

    first commit

---
+++ src/app/api/auth/[kindAuth]/route.ts
@@ -0,0 +1,13 @@
+import { AuthEndpoints, handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
+
+interface Props {
+  params: {
+    kindAuth: string;
+  };
+}
+
+export async function GET(request: NextRequest, { params }: Props) {
+  const endpoint = params.kindAuth;
+  return handleAuth(request, endpoint);
+}
